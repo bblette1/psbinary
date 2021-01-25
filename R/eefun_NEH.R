adc7446cdaf48d40d7f2ebdc13a50a4cf63e3416
Bryan Blette <bletteb18@gmail.com>
2021-01-25 09:24:37 -0500

    Add skeleton for eefun_NEH()

---
+++ R/eefun_NEH.R
@@ -0,0 +1,7 @@
+# Estimating equation vector for ignorance interval under NEB
+# Used for M estimation variance calculation
+eefun_NEB <- function(data, beta0range, beta1range, beta2range, beta3range,
+                      contrast, design, whichmin_00, whichmax_00, whichmin_10,
+                      whichmax_10, whichmin_diff, whichmax_diff) {
+
+}
